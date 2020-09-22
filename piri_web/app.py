647980bf380f453d31af359b7dae54141401e8ea
Dmytro Chasovskyi <dmytro.chasovskyi@greenbird.com>
2020-09-22 17:29:07 +0200

    Add more graceful error logging

--- piri_web/app.py
+++ piri_web/app.py
@@ -2,8 +2,25 @@ import json
 
 import falcon
 from attr import dataclass
-from piri.process import process
+from falcon import Response
+from piri.process import Process
 from piri.schema import SCHEMA
+from returns.result import safe
+
+
+@safe
+def create_success_response(*args, **kwargs) -> Response:
+    """Return success response."""
+    return (args, falcon.HTTP_200)
+
+
+@safe
+def create_failure_response(failure: Exception) -> Response:
+    """Return failure response."""
+    error_message = '{failure}'.format(
+        failure=failure.args[0],
+    )
+    return ({'error': error_message}, falcon.HTTP_400)
 
 
 @dataclass(frozen=True, slots=True)
@@ -24,9 +41,17 @@ class Mapper(object):
 
         post_data = json.loads(req.stream.read(req.content_length))
 
-        resp.body = json.dumps(
-            process(post_data['data'], post_data['configuration']),
-        )
+        body, code = Process()(
+            post_data['data'],
+            post_data['configuration'],
+        ).bind(
+            create_failure_response,
+        ).rescue(
+            create_failure_response,
+        ).unwrap()
+
+        resp.body = json.dumps(body)
+        resp.status = code
 
 
 application = falcon.API()
